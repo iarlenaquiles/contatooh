2df2f56d935cc4f430b688bb9c7860322ef465af
Iarlen <iarlensilva775@gmail.com>
2017-06-03 19:06:51 -0300

    criando a instancia do servidor e configurando o express

--- config/express.js
+++ config/express.js
@@ -1,1 +1,9 @@
 var express = require('express');
+module.exports = function() {
+	var app = express();
+	//configuração de ambiente
+	app.set('port', 3000);
+
+	app.use(express.static('./public'));
+	return app;
+};
