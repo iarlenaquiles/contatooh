53136d8009c9911718b364cec3b8bda0b422dfb1
Iarlen <iarlensilva775@gmail.com>
2017-06-14 08:54:19 -0300

    Populando os contatos no back-end

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,6 +1,12 @@
 var contatos = [
 {_id:1, nome: 'Iarlen', email: 'iarlem@gmail.com'},
-{_id:2, nome: 'Jonas', email: 'jonas@gmail.com'}
+{_id:2, nome: 'Jonas', email: 'jonas@gmail.com'},
+{_id:3, nome: 'Joao', email: 'joao@gmail.com'},
+{_id:4, nome: 'Antoin', email: 'antoin@gmail.com'},
+{_id:5, nome: 'Italo', email: 'italo@gmail.com'},
+{_id:6, nome: 'Rodrigo', email: 'rodrigo@gmail.com'},
+{_id:7, nome: 'Messias', email: 'messias@gmail.com'},
+{_id:8, nome: 'Jack', email: 'jack@gmail.com'}
 ];
 module.exports = function(){
 	var controller = {};
