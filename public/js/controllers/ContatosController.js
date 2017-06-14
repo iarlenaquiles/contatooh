29384de0ec04f209b247752485b187eea22be507
Iarlen <iarlensilva775@gmail.com>
2017-06-13 21:38:59 -0300

    Integração do Express com Angular

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,24 +1,21 @@
 angular.module('contatooh').controller('ContatosController', 
-	function($scope){
+	function($scope, $http){
 		$scope.total = 0;
+
+		$scope.contatos = [];
+
+		$scope.filtro = '';
+
 		$scope.incrementa = function(){
 			$scope.total++;
 		};
 
-		$scope.contatos = [
-			{"_id": 1,
-			"nome": "Contato Angular 1",
-			"email": "cont1@empresa.com.br"
-			},
-			{"_id": 2,
-			"nome": "Contato Angular 2",
-			"email": "cont2@empresa.com.br"
-			},
-			{"_id": 3,
-			"nome": "Contato Angular 3",
-			"email": "cont3@empresa.com.br"
-			}
-		];
-
-		$scope.filtro = '';
+		$http.get('/contatos')
+		.success(function(data){
+			$scope.contatos = data;
+		})
+		.error(function(statusText){
+			console.log("Não foi possível obter a lista de contatos");
+			console.log(statusText);
+		});
 	});
