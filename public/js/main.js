54b566518f8b560c90654d02d9dcecd3a035b17a
Iarlen <iarlensilva775@gmail.com>
2017-06-14 22:11:55 -0300

    Alterando o controller com contato e usando a diretiva ng-href

--- public/js/main.js
+++ public/js/main.js
@@ -9,10 +9,5 @@ angular.module('contatooh', ['ngRoute', 'ngResource']).config(function($routePro
 		controller: 'ContatoController'
 	});
 
-	$routeProvider.when('/contato', {
-		templateUrl: 'partials/contato.html',
-		controller: 'ContatoController'
-	});
-
 	$routeProvider.otherwise({redirectTo: '/contatos'});
 });
