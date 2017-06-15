3f66757e4f8272b044bca0e98c922252d8efabf7
Iarlen <iarlensilva775@gmail.com>
2017-06-14 22:20:59 -0300

    Criando o botão para redirecionar para o form de contato

--- public/js/controllers/ContatoController.js
+++ public/js/controllers/ContatoController.js
@@ -1,13 +1,18 @@
 angular.module('contatooh').controller('ContatoController', 
 	function($scope, $routeParams, $resource){
 		var Contato = $resource('/contatos/:id');
-		Contato.get({id: $routeParams.contatoId},
-			function(contato){
-				$scope.contato = contato;
-			},
-			function(erro){
-				$scope.mensagem = {
-					texto: 'Não foi possível obter o contato.'
-				};
-			});
+
+		if($routeParams.contatoId){
+			Contato.get({id: $routeParams.contatoId},
+				function(contato){
+					$scope.contato = contato;
+				},
+				function(erro){
+					$scope.mensagem = {
+						texto: 'Não foi possível obter o contato.'
+					};
+				});
+		}else{
+			$scope.contato = {};
+		}
 	});
