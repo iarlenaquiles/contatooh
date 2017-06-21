ba213b2deb31800c3a28c5230a9a100b12e5dc38
Iarlen <iarlensilva775@gmail.com>
2017-06-20 22:04:56 -0300

    Criando o metodo no controller do contato

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,15 +1,7 @@
-var contatos = [
-{_id:1, nome: 'Iarlen', email: 'iarlem@gmail.com'},
-{_id:2, nome: 'Jonas', email: 'jonas@gmail.com'},
-{_id:3, nome: 'Joao', email: 'joao@gmail.com'},
-{_id:4, nome: 'Antoin', email: 'antoin@gmail.com'},
-{_id:5, nome: 'Italo', email: 'italo@gmail.com'},
-{_id:6, nome: 'Rodrigo', email: 'rodrigo@gmail.com'},
-{_id:7, nome: 'Messias', email: 'messias@gmail.com'},
-{_id:8, nome: 'Jack', email: 'jack@gmail.com'}
-];
+var contatos = [];
 module.exports = function(){
 	var controller = {};
+	var id_contato_inc=0;
 	controller.listaContatos = function(req, res){
 		res.json(contatos);
 	};
@@ -31,6 +23,29 @@ module.exports = function(){
 
 		res.status(204).end();
 	};
+
+	controller.salvaContato = function(req, res){
+		var contato = req.body;
+		contato = contato._id ? atualiza(contato) : adiciona(contato);
+		res.json(contato);
+	};
+
+	function adiciona(contatoNovo){
+		contatoNovo._id = ++id_contato_inc;
+		contatos.push(contatoNovo);
+		return contatoNovo;
+	};
+
+	function atualiza(contatoAlterar){
+		contatos = contatos.map(function(contato){
+			if(contato._id == contatoAlterar._id){
+				contato = contatoAlterar;
+			}
+			return contato;
+		});
+
+		return contatoAlterar;
+	}
 
 	return controller;
 };
