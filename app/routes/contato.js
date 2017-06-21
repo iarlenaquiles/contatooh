515e4aabe3674dc60c732bf91742658e6fac506e
Iarlen <iarlensilva775@gmail.com>
2017-06-20 21:54:14 -0300

    Criando a rota do salvamento

--- app/routes/contato.js
+++ app/routes/contato.js
@@ -1,5 +1,5 @@
 module.exports = function(app){
 	var controller = app.controllers.contato;
-	app.route('/contatos').get(controller.listaContatos);
+	app.route('/contatos').get(controller.listaContatos).post(controller.salvaContato);
 	app.route('/contatos/:id').get(controller.obtemContato).delete(controller.removeContato);
 }
