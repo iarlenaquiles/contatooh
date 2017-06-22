b8d132a3522a01e1dfbd87d4f5d703096ab2ca70
Iarlen <iarlensilva775@gmail.com>
2017-06-22 19:47:04 -0300

    Finalizando a configuração do banco

--- config/database.js
+++ config/database.js
@@ -14,4 +14,11 @@ module.exports = function(uri){
 	mongoose.connection.on('error', function(){
 		console.log('Mongoose! Erro na conexão: ' + uri);
 	});
+
+	process.on('SIGNIT', function() {
+		mongoose.connection.close(function(){
+			console.log('Mongoose! Desconectado pelo término da aplicação!');
+			process.exit(0);
+		});
+	});
 }
