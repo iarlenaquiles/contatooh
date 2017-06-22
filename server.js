188395f316442ebc4d235e5e180513f44a35eadf
Iarlen <iarlensilva775@gmail.com>
2017-06-22 19:42:44 -0300

    Finalizando a configuração do banco

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require('http');
 
 var app = require('./config/express')();
 
+require('./config/database.js')('mongodb://localhost/contatooh');
+
 http.createServer(app).listen(app.get('port'), function () {
 	console.log('Express Server executando a porta ' + app.get('port'));
 });
