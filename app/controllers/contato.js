11add8ef559783df2309e35376ddefacf1cf6dae
Iarlen <iarlensilva775@gmail.com>
2017-06-24 17:43:24 -0300

    Refatorando o contato controller

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -5,14 +5,7 @@ module.exports = function () {
 		res.json(contatos);
 	};
 
-	controller.obtemContato = function (req, res) {
-		var idContato = req.params.id;
-		var contato = contatos.filter(function (contato) {
-			return contato._id == idContato;
-		})[0];
-
-		contato ? res.json(contato) : res.status(404).send('Contato não encontrado');
-	};
+	controller.obtemContato = function (req, res) {};
 
 	controller.removeContato = function (req, res) {
 		var idContato = req.params.id;
